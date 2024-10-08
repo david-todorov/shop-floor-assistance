51ba8c04f0287396a7443aae240d971dee92de58
david <david.todorov@profect.de>
2024-10-08 23:11:25 +0200

    Backend init + maven checkstyle added

---
+++ backend/shop-floor-assistance-backend/src/test/java/com/uhlmann/shopfloor/shopfloorassistancebackend/ShopFloorAssistanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhlmann.shopfloor.shopfloorassistancebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopFloorAssistanceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
