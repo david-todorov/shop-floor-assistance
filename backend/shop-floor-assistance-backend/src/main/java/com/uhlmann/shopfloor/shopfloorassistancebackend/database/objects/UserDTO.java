a3de713d26949e0304d58950ef029254844515ac
david <david.todorov@profect.de>
2024-10-13 02:37:11 +0200

    Unnecessary columns removed from UserDTO and api point /workflows/editor POST, removed the parameter for testing purposes.

--- backend/shop-floor-assistance-backend/src/main/java/com/uhlmann/shopfloor/shopfloorassistancebackend/database/objects/UserDTO.java
+++ backend/shop-floor-assistance-backend/src/main/java/com/uhlmann/shopfloor/shopfloorassistancebackend/database/objects/UserDTO.java
@@ -4,13 +4,10 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.sql.Date;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
