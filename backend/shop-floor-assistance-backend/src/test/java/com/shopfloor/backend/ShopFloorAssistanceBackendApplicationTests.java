9590300b089a48d4f2acea598c744fd0e41dbd04
david <david.todorov@profect.de>
2024-10-18 00:40:14 +0200

    Removed tests

--- backend/shop-floor-assistance-backend/src/test/java/com/shopfloor/backend/ShopFloorAssistanceBackendApplicationTests.java
+++ backend/shop-floor-assistance-backend/src/test/java/com/shopfloor/backend/ShopFloorAssistanceBackendApplicationTests.java
@@ -1,9 +1,8 @@
 package com.shopfloor.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ShopFloorAssistanceBackendApplicationTests {
 
     @Test
