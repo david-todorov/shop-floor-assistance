9250689a6ea9ea1a528b1f89b3d058bb5a7a23dc
joss-thu <antojo01@thu.de>
2024-10-23 18:15:17 +0200

    saving state

--- frontend/shop-floor-assistance-frontend/src/app/editor-dashboard/editor-dashboard.component.ts
+++ frontend/shop-floor-assistance-frontend/src/app/editor-dashboard/editor-dashboard.component.ts
@@ -1,16 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import { sampleOrders } from '../types/dummyData';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { orderTO } from '../types/orderTO';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-editor-dashboard',
   standalone: true,
-  imports: [RouterLink, RouterModule, CommonModule],
+  imports: [RouterLink, RouterModule, CommonModule, 
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    FormsModule],
   templateUrl: './editor-dashboard.component.html',
   styleUrl: './editor-dashboard.component.css',
   providers:[RouterLink, RouterModule]
 })
 export class EditorDashboardComponent {
   constructor(private routerLink: RouterLink){}
+  orders = sampleOrders;
+  displayedColumns: string[] = ['orderNumber', 'name', 'shortDescription'];
+
 
 }
