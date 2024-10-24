e48bf7221efb19f909cc15857570cbf0b062484a
Citlalin Reyes <citlalin.reyessoto@gmail.com>
2024-10-24 07:47:26 +0200

    Ehanced Table for Editor dashboard

--- frontend/shop-floor-assistance-frontend/src/app/editor-dashboard/editor-dashboard.component.ts
+++ frontend/shop-floor-assistance-frontend/src/app/editor-dashboard/editor-dashboard.component.ts
@@ -3,13 +3,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 import { sampleOrders } from '../types/dummyData';
 import { MatTableModule } from '@angular/material/table';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { orderTO } from '../types/orderTO';
-import { MatCheckbox } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { ButtonComponent } from '../components/button/button.component';
 
 @Component({
   selector: 'app-editor-dashboard',
@@ -18,7 +17,9 @@ import { FormsModule } from '@angular/forms';
     MatTableModule,
     MatSortModule,
     MatPaginatorModule,
-    FormsModule],
+    FormsModule,
+    MatButtonModule,
+    ButtonComponent],
   templateUrl: './editor-dashboard.component.html',
   styleUrl: './editor-dashboard.component.css',
   providers:[RouterLink, RouterModule]
