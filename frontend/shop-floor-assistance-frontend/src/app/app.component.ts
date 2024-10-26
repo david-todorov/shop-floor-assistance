f4a47344935b36b04fc316bbef0b9175f5907572
joss-thu <antojo01@thu.de>
2024-10-26 07:53:13 +0200

    Reorganizing: removed bloat code

--- frontend/shop-floor-assistance-frontend/src/app/app.component.ts
+++ frontend/shop-floor-assistance-frontend/src/app/app.component.ts
@@ -1,21 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AuthService } from './service/auth.service';
-import { LoginComponent } from './login/login/login.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { OperatorAssistanceComponent } from './operator-assistance/operator-assistance.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, RouterLink, RouterModule, LoginComponent,
-    HttpClientModule, OperatorAssistanceComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [AuthService]
+  providers: []
 })
 export class AppComponent {
-  title = 'shop-floor-assistance-frontend';
-  constructor(private authService: AuthService) { }
+  title = 'Digital Workflow Assistant';
+  constructor() { }
 }
