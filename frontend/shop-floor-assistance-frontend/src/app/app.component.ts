dfb908f0ba9188fa234ec1760ae9661870397db4
joss-thu <antojo01@thu.de>
2024-10-28 23:21:35 +0100

    Homepage layout.

--- frontend/shop-floor-assistance-frontend/src/app/app.component.ts
+++ frontend/shop-floor-assistance-frontend/src/app/app.component.ts
@@ -5,12 +5,19 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatSlideToggleModule, MatSliderModule],
+  imports: [MatSlideToggleModule, 
+    MatSliderModule, 
+    MatToolbarModule, 
+    MatIconModule,
+  RouterOutlet,
+RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: []
