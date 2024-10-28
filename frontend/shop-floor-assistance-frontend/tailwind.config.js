c838c7999002edbbf9b0f0bf396bdfe1e840f47c
joss-thu <antojo01@thu.de>
2024-10-28 17:53:52 +0100

    custom style themes defined in tailwind.config.ts, sfa-styles.scss and styles.css. angular.json updated accordingly.

---
+++ frontend/shop-floor-assistance-frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{html,js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0477BF',
+        accentLight: '#66ADD9',
+        background: '#F2F2F2',
+        shade: '#595959',
+        accentDark:'#0D0D0D'
+      },
+      fontFamily: {
+        body: ['Nunito'],
+      }
+    },
+  },
+  plugins: [],
+}
+
