edc61d300471a79ef6e2723a80f60a8ba50763f6
joss-thu <antojo01@thu.de>
2024-10-28 17:55:37 +0100

    editor, operator components added. auth.service and backend-communication services to be refactored.

---
+++ frontend/shop-floor-assistance-frontend/src/app/components/operator/operator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operator',
+  standalone: true,
+  imports: [],
+  templateUrl: './operator.component.html',
+  styleUrl: './operator.component.css'
+})
+export class OperatorComponent {
+
+}
