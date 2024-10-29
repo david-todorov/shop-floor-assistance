2cde5258891f48595390cfff3a9e233bd452e78b
joss-thu <antojo01@thu.de>
2024-10-29 12:57:44 +0100

    log in (layout only) set.

--- frontend/shop-floor-assistance-frontend/src/app/app.routes.ts
+++ frontend/shop-floor-assistance-frontend/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { EditorComponent } from './components/editor/editor.component';
+import { LoginRegisterComponent } from './components/login-register/login-register.component';
 
 export const routes: Routes = [
     {path: '', component:HomeComponent},
-    {path: 'editor', component:EditorComponent}
+    {path: 'login', component:LoginRegisterComponent}
 ];
