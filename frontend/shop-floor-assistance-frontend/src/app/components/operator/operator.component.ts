e3cc4d10e3a70e02188d32565facc3cb0cddf898
joss-thu <antojo01@thu.de>
2024-10-30 21:46:15 +0100

    order select and start wizard: essential functionality

--- frontend/shop-floor-assistance-frontend/src/app/components/operator/operator.component.ts
+++ frontend/shop-floor-assistance-frontend/src/app/components/operator/operator.component.ts
@@ -2,15 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { OrderTableComponent } from '../../shared/component-elements/order-table/order-table.component';
 import { orderTO } from '../../types/orderTO';
 import { sampleOrders } from '../../types/dummyData';
+import { ButtonComponent } from '../../shared/component-elements/button/button.component';
 
 @Component({
   selector: 'app-operator',
   standalone: true,
-  imports: [OrderTableComponent],
+  imports: [OrderTableComponent,
+    ButtonComponent
+  ],
   templateUrl: './operator.component.html',
   styleUrl: './operator.component.css'
 })
 export class OperatorComponent implements OnInit{
+
+  btnLabel: string= 'Start Wizard';
+  order!: orderTO;
 
   ngOnInit(): void {
     this.loadedOrders= sampleOrders;
@@ -18,11 +24,14 @@ export class OperatorComponent implements OnInit{
   
   loadedOrders!: orderTO[];
 
+  orderSelected($event: any) {
+    this.order= $event
+  }
 
   resolveButtonClick($event: any) {
-
-  console.log($event)
-
-}
+    if($event.type==='click' && this.order !== null && this.order !== undefined){
+      console.log(this.order);
+    }
+  }
 
 }
