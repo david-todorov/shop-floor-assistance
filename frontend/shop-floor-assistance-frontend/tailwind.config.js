82232dd66a57a9a756b35dca2b647964f6124cd0
joss-thu <antojo01@thu.de>
2024-10-31 09:47:48 +0100

    UI: added scrollbar to the order-table element

--- frontend/shop-floor-assistance-frontend/tailwind.config.js
+++ frontend/shop-floor-assistance-frontend/tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
         accent: '#66ADD9',
         background: '#F2F2F2',
         shade: '#595959',
-        dark:'#0D0D0D'
+        dark:'#0D0D0D',
+                scrollbar: {
+          DEFAULT: '#66ADD9', // Default scrollbar color
+          hover: '#0477BF', // Hover state scrollbar color
+        },
       }
     },
   },
