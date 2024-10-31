a3c9afbe17c37e7003ecc2a456a7c6f0a426db15
joss-thu <antojo01@thu.de>
2024-10-31 11:18:27 +0100

    order-table functionality stable

--- frontend/shop-floor-assistance-frontend/src/app/shared/component-elements/order-table/order-table.component.ts
+++ frontend/shop-floor-assistance-frontend/src/app/shared/component-elements/order-table/order-table.component.ts
@@ -1,9 +1,11 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, inject, Input, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { orderTO } from '../../../types/orderTO';
 
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-order-table',
@@ -12,22 +14,25 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
     MatTableModule,
     MatRadioModule,
     MatPaginatorModule,
+    MatSortModule,
   ],
   templateUrl: './order-table.component.html',
   styleUrl: './order-table.component.css',
 })
 export class OrderTableComponent implements OnInit, AfterViewInit{
 
-
   @Input() orders!: orderTO[];
   @Output() onClick = new EventEmitter<any>();
 
   dataSource!:MatTableDataSource<orderTO>;
+  // @ViewChild(MatSort) sort!: MatSort;
 
   constructor(){}
 
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource<orderTO>(this.orders);
+    // this.dataSource.sort = this.sort;
+    
   }
 
   displayedColumns: string[] = ['select', 'Order No.', 'Name', 'Description', ];
@@ -35,7 +40,9 @@ export class OrderTableComponent implements OnInit, AfterViewInit{
   
   // Paginator if required
   // @ViewChild(MatPaginator) paginator!: MatPaginator;
-  ngAfterViewInit() {// this.dataSource.paginator = this.paginator;
+  ngAfterViewInit() {
+    // this.dataSource.paginator = this.paginator;
+    // this.dataSource.sort = this.sort;
     }
 
   onRadioChange(order: orderTO) {
