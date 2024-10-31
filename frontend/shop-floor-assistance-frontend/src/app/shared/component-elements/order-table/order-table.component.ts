f84ff01fa435b4f928c3f9d9923572bdcd023169
joss-thu <antojo01@thu.de>
2024-10-31 10:55:50 +0100

    search functionality implemented on order tables.

--- frontend/shop-floor-assistance-frontend/src/app/shared/component-elements/order-table/order-table.component.ts
+++ frontend/shop-floor-assistance-frontend/src/app/shared/component-elements/order-table/order-table.component.ts
@@ -1,29 +1,50 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MatRadioModule } from '@angular/material/radio';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { orderTO } from '../../../types/orderTO';
-import { sampleOrders } from '../../../types/dummyData';
+
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-order-table',
   standalone: true,
-  imports: [MatTableModule,
-    MatRadioModule
+  imports: [
+    MatTableModule,
+    MatRadioModule,
+    MatPaginatorModule,
   ],
   templateUrl: './order-table.component.html',
   styleUrl: './order-table.component.css',
 })
-export class OrderTableComponent implements OnInit{
+export class OrderTableComponent implements OnInit, AfterViewInit{
+
 
   @Input() orders!: orderTO[];
   @Output() onClick = new EventEmitter<any>();
 
+  dataSource!:MatTableDataSource<orderTO>;
+
   constructor(){}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<orderTO>(this.orders);
+  }
 
   displayedColumns: string[] = ['select', 'Order No.', 'Name', 'Description', ];
+
+  
+  // Paginator if required
+  // @ViewChild(MatPaginator) paginator!: MatPaginator;
+  ngAfterViewInit() {// this.dataSource.paginator = this.paginator;
+    }
+
   onRadioChange(order: orderTO) {
     this.onClick.emit(order);
   }
+
+  applyFilter($event: Event) {
+      const filterValue = ($event.target as HTMLInputElement).value;
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
