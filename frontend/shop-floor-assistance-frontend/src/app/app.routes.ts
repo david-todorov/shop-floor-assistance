00b2d935e64e4e9c0bc103af32a5c28bae48f677
joss-thu <antojo01@thu.de>
2024-11-02 09:26:21 +0100

    UI: start wizard after slecting workflow-> new operator/{order-name} page To do: display actual workflow.

--- frontend/shop-floor-assistance-frontend/src/app/app.routes.ts
+++ frontend/shop-floor-assistance-frontend/src/app/app.routes.ts
@@ -4,10 +4,13 @@ import { EditorComponent } from './components/editor/editor.component';
 import { LoginRegisterComponent } from './components/login-register/login-register.component';
 import { AppComponent } from './app.component';
 import { OperatorComponent } from './components/operator/operator.component';
+import { EditorAccordionComponent } from './shared/component-elements/editor-accordion/editor-accordion.component';
+import { OperatorViewWorkflowComponent } from './components/operator-view-workflow/operator-view-workflow.component';
 
 export const routes: Routes = [
     {path: '', component:WelcomeComponent},
     {path: 'login', component:LoginRegisterComponent},
     {path: 'editor', component:EditorComponent},
-    {path: 'operator', component:OperatorComponent},
+    {path: 'operator', component:OperatorComponent,},
+    {path: 'operator/:id', component:OperatorViewWorkflowComponent,},
 ];
