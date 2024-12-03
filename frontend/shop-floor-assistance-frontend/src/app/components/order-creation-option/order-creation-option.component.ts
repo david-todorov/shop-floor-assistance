5eeca727b8d7889447e8f54600c6458fed8cba6b
Citlalin Reyes <citlalin.reyessoto@gmail.com>
2024-12-03 13:05:10 +0100

    Add comment to Order-creation-option

--- frontend/shop-floor-assistance-frontend/src/app/components/order-creation-option/order-creation-option.component.ts
+++ frontend/shop-floor-assistance-frontend/src/app/components/order-creation-option/order-creation-option.component.ts
@@ -12,16 +12,17 @@ import { ButtonComponent } from "../../shared/component-elements/button/button.c
 export class OrderCreationOptionComponent {
   constructor(private router: Router) {}
 
+  // Labels for the buttons
   createBtnLabel: string = 'From template';
   createNewBtnLabel: string = 'Create new';
 
+  // Method to navigate to "Create from Scratch" component
   createFromScratch() {
-    // Navigate to the "Create from Scratch" component
     this.router.navigateByUrl('/editor-orders/create');
   }
 
+  // Method to navigate to the "Create from Existing" component
   createFromExisting() {
-    // Navigate to the "Create from Existing" component
     this.router.navigateByUrl('/editor-orders/create-from-existing');
   }
 }
