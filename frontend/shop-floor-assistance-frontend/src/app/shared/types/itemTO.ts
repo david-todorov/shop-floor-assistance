c7e1bddba9d62fa3ee64d5ae7c97866250f1f141
Ebtehal Aldheeshy <ebtehal19@gmail.com>
2024-12-03 09:50:23 +0100

    Add comment in itemTO

--- frontend/shop-floor-assistance-frontend/src/app/shared/types/itemTO.ts
+++ frontend/shop-floor-assistance-frontend/src/app/shared/types/itemTO.ts
@@ -1,11 +1,10 @@
 export type itemTO = {
-  id?: number;
-  name: string;
-  description?: string;
-  timeRequired?: number| null;  
-  createdBy?: number;
-  updatedBy?: number;
-  createdAt?: Date;
-  updatedAt?: Date;
- 
+  id?: number; // Optional: Unique identifier for the item
+  name: string; // Name of the item (required)
+  description?: string; // Optional: Additional details or description of the item
+  timeRequired?: number | null; // Optional: Estimated time required for the item (in minutes or seconds), can be null if not applicable
+  createdBy?: number; // Optional: Identifier of the user who created the item
+  updatedBy?: number; // Optional: Identifier of the user who last updated the item
+  createdAt?: Date; // Optional: Timestamp indicating when the item was created
+  updatedAt?: Date; // Optional: Timestamp indicating when the item was last updated
 };
