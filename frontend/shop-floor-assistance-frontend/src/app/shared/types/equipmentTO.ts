dd22ac00a7c470e436718190754ce17a1bb58ec2
Ebtehal Aldheeshy <ebtehal19@gmail.com>
2024-12-03 09:49:40 +0100

    Add comment in equipmentTO

--- frontend/shop-floor-assistance-frontend/src/app/shared/types/equipmentTO.ts
+++ frontend/shop-floor-assistance-frontend/src/app/shared/types/equipmentTO.ts
@@ -1,12 +1,12 @@
 export type equipmentTO = {
-    id?: number;
-    equipmentNumber: string;            // To match equipmentNumber from backend
-    name: string;
-    type: string;             
-    description: string;
-    createdBy?: string;        
-    updatedBy?: string;       
-    createdAt?: Date;          
-    updatedAt?: Date;          
-    orders?: [];            
+    id?: number; // Optional: Unique identifier for the equipment
+    equipmentNumber: string; // Equipment number, used to match with backend data
+    name: string; // Name of the equipment
+    type: string; // Type/category of the equipment
+    description: string; // Description providing details about the equipment
+    createdBy?: string; // Optional: Username or identifier of the creator
+    updatedBy?: string; // Optional: Username or identifier of the last person who updated the equipment
+    createdAt?: Date; // Optional: Timestamp of when the equipment was created
+    updatedAt?: Date; // Optional: Timestamp of the last update made to the equipment
+    orders?: []; // Optional: Array to hold associated orders, if applicable
 };
