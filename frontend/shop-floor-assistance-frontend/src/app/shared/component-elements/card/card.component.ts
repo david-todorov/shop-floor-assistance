374bdf4b074f391495517da947a38d21b291a206
Ebtehal Aldheeshy <ebtehal19@gmail.com>
2024-12-03 09:27:04 +0100

    Add Comment in card

--- frontend/shop-floor-assistance-frontend/src/app/shared/component-elements/card/card.component.ts
+++ frontend/shop-floor-assistance-frontend/src/app/shared/component-elements/card/card.component.ts
@@ -1,13 +1,15 @@
+// Importing necessary Angular modules
 import { Component } from '@angular/core';
+// Importing Angular Material Card module for card layout and styling
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
-  selector: 'app-card',
-  standalone: true,
-  imports: [MatCardModule],
-  templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  selector: 'app-card', // Selector for using this component in templates
+  standalone: true, // Indicates this component is standalone and does not rely on a module
+  imports: [MatCardModule], // Declares the dependencies used in this component
+  templateUrl: './card.component.html', // Path to the HTML template for this component
+  styleUrl: './card.component.css' // Path to the CSS styles for this component
 })
-export class CardComponent {
-
+export class CardComponent {
+  // This is a simple Angular component that leverages Angular Material's MatCardModule
 }
