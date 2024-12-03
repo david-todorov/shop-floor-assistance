63746595b70a2f2bb3871b7311570bd6a935b32e
Citlalin Reyes <citlalin.reyessoto@gmail.com>
2024-12-03 13:08:39 +0100

    Add comment to Product-creation-option

--- frontend/shop-floor-assistance-frontend/src/app/components/product-creation-option/product-creation-option.component.ts
+++ frontend/shop-floor-assistance-frontend/src/app/components/product-creation-option/product-creation-option.component.ts
@@ -12,16 +12,17 @@ import { ButtonComponent } from "../../shared/component-elements/button/button.c
 export class ProductCreationOptionComponent {
   constructor(private router: Router) {}
 
+  // Labels for the buttons
   createBtnLabel: string = 'From template';
   createNewBtnLabel: string = 'Create new';
 
+  // Method to navigate to the "Create from Scratch" component
   createFromScratch() {
-    // Navigate to the "Create from Scratch" component
     this.router.navigateByUrl('/editor-product/create');
   }
 
+  // Method to navigate to the "Create from Existing" component
   createFromExisting() {
-    // Navigate to the "Create from Existing" component
     this.router.navigateByUrl('/editor-product/create-from-existing');
   }
 }
